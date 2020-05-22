c16cb5be4369c7157a8921894a356488a6ea1345
akoen <arw.koen@gmail.com>
2020-05-22 12:02:49 -0700

    Phonebook backend step6

--- index.js
+++ index.js
@@ -42,39 +42,40 @@ app.get("/info", (req, res) => {
 
 app.get("/api/persons/:id", (req, res) => {
   const id = +req.params.id;
-  const person = persons.find(i => i.id === id);
+  const person = persons.find((i) => i.id === id);
 
-  if(person)
-    res.json(person);
-  else
-    res.status(404).end();
+  if (person) res.json(person);
+  else res.status(404).end();
 });
 
 // DELETE requests
 app.delete("/api/persons/:id", (req, res) => {
   const id = +req.params.id;
 
-  persons = persons.filter(i => i.id !== id);
+  persons = persons.filter((i) => i.id !== id);
 
   res.status(204).end();
-  
-})
+});
 
 // POST requests
-app.post('/api/persons', (req, res) => {
-
+app.post("/api/persons", (req, res) => {
   const body = req.body;
-  
-  const person = {
-    ...body,
-    id: Math.floor(Math.random() * 1000),
+
+  if (persons.find((i) => i.name === body.name)) {
+    res.status(400).json({ error: "name must be unique" });
+  } else if (persons.find((i) => i.number === body.number)) {
+    res.status(400).json({ error: "number must be unique" });
+  } else {
+    const person = {
+      ...body,
+      id: Math.floor(Math.random() * 1000),
+    };
+
+    persons = persons.concat(person);
+
+    res.json(person);
   }
-
-  persons = persons.concat(person);
-
-  res.json(person);
-
-})
+});
 
 app.listen(PORT);
 console.log(`Server running on ${PORT}`);
