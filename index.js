4039ba2da3f18cb6fdafcfbb1f5f2193ce2cc869
akoen <arw.koen@gmail.com>
2020-05-22 09:49:07 -0700

    Phonebook backend step1

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3001;
+
+const persons = [
+  {
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1,
+  },
+  {
+    name: 'Ada Lovelace',
+    number: '123',
+    id: 2,
+  },
+  {
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+    id: 3,
+  },
+  {
+    name: 'Marry Poppendieck',
+    number: '39-23-6423122',
+  },
+];
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
+});
+
+app.listen(PORT);
+console.log(`Server running on ${PORT}`);
