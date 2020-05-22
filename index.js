e4362ec9e8c59ebeb2b106f0f670642249d2f508
akoen <arw.koen@gmail.com>
2020-05-22 10:16:07 -0700

    Phonebook backend step3

--- index.js
+++ index.js
@@ -1,38 +1,52 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = 3001;
 
 const persons = [
   {
-    name: 'Arto Hellas',
-    number: '040-123456',
+    name: "Arto Hellas",
+    number: "040-123456",
     id: 1,
   },
   {
-    name: 'Ada Lovelace',
-    number: '123',
+    name: "Ada Lovelace",
+    number: "123",
     id: 2,
   },
   {
-    name: 'Dan Abramov',
-    number: '12-43-234345',
+    name: "Dan Abramov",
+    number: "12-43-234345",
     id: 3,
   },
   {
-    name: 'Marry Poppendieck',
-    number: '39-23-6423122',
+    name: "Marry Poppendieck",
+    number: "39-23-6423122",
+    id: 4,
   },
 ];
 
-app.get('/api/persons', (req, res) => {
+// GET requests
+app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
-app.get('/info', (req, res) => {
+app.get("/info", (req, res) => {
   const date = new Date();
-  res.send(`Phonebook has info for ${persons.length} people. <br/><br/>${date}`)
-})
+  res.send(
+    `Phonebook has info for ${persons.length} people. <br/><br/>${date}`
+  );
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = +req.params.id;
+  const person = persons.find(i => i.id === id);
+
+  if(person)
+    res.json(person);
+  else
+    res.status(404).end();
+});
 
 app.listen(PORT);
 console.log(`Server running on ${PORT}`);
