15188efea4d1cd85ac025e7c07a2b809b7cdbc31
akoen <arw.koen@gmail.com>
2020-05-26 11:44:07 -0700

    Phonebook backend step7

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
-const express = require("express");
-const app = express();
+const express = require('express');
+const morgan = require('morgan');
 
+const app = express();
+app.use(morgan('tiny'));
 app.use(express.json());
 
 const PORT = 3001;
 
 let persons = [
   {
-    name: "Arto Hellas",
-    number: "040-123456",
+    name: 'Arto Hellas',
+    number: '040-123456',
     id: 1,
   },
   {
-    name: "Ada Lovelace",
-    number: "123",
+    name: 'Ada Lovelace',
+    number: '123',
     id: 2,
   },
   {
-    name: "Dan Abramov",
-    number: "12-43-234345",
+    name: 'Dan Abramov',
+    number: '12-43-234345',
     id: 3,
   },
   {
-    name: "Marry Poppendieck",
-    number: "39-23-6423122",
+    name: 'Marry Poppendieck',
+    number: '39-23-6423122',
     id: 4,
   },
 ];
 
 // GET requests
-app.get("/api/persons", (req, res) => {
+app.get('/api/persons', (req, res) => {
   res.json(persons);
 });
 
-app.get("/info", (req, res) => {
+app.get('/info', (req, res) => {
   const date = new Date();
-  res.send(
-    `Phonebook has info for ${persons.length} people. <br/><br/>${date}`
-  );
+  res.send(`Phonebook has info for ${persons.length} people. <br/><br/>${date}`);
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get('/api/persons/:id', (req, res) => {
   const id = +req.params.id;
   const person = persons.find((i) => i.id === id);
 
@@ -49,7 +49,7 @@ app.get("/api/persons/:id", (req, res) => {
 });
 
 // DELETE requests
-app.delete("/api/persons/:id", (req, res) => {
+app.delete('/api/persons/:id', (req, res) => {
   const id = +req.params.id;
 
   persons = persons.filter((i) => i.id !== id);
@@ -58,13 +58,13 @@ app.delete("/api/persons/:id", (req, res) => {
 });
 
 // POST requests
-app.post("/api/persons", (req, res) => {
+app.post('/api/persons', (req, res) => {
   const body = req.body;
 
   if (persons.find((i) => i.name === body.name)) {
-    res.status(400).json({ error: "name must be unique" });
+    res.status(400).json({ error: 'name must be unique' });
   } else if (persons.find((i) => i.number === body.number)) {
-    res.status(400).json({ error: "number must be unique" });
+    res.status(400).json({ error: 'number must be unique' });
   } else {
     const person = {
       ...body,
