78efda087ac3e69b45b4540b14273ab40e65075a
akoen <arw.koen@gmail.com>
2020-06-16 11:19:53 -0700

    Added build

---
+++ build/precache-manifest.6163e1d76d74005146e13b59cc0a3157.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30ab2e6bd223cd44649ba4bda58d7634",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49bdc01c5b65f8d5ccd5",
+    "url": "/static/js/2.3b9df0ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3b9df0ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61784553bcdfd27730d4",
+    "url": "/static/js/main.37b81333.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
